2d2058dc6e1cb3324d9ae3b25577a68435e234f9
z-miy-nagaya@taitan.jp <z-miy-nagaya@taitan.jp>
2018-05-07 14:53:57 +0900

    add to

---
+++ buildsettings.js
@@ -0,0 +1,7 @@
+/* テーマ基本設定 */
+themeName = '[案件名]テンプレート';
+themeURL = 'https://github.com/miyako-n/newtheme-package';
+themaFolder = 'test';
+themeDomein ='http://newtheme.test';
+themeAuthor ='Miyaka[KinoDesigns]';
+themeVersion: '1.0.0';
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,50 @@
 require('es6-promise').polyfill();
 
-var path = require('path');
+var base = require('buildsettings.js');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-mozjpeg');
+var mozjpeg = require('imagemin-mozjpeg');
+
+//Publish
+gulp.task('publish', function() {
+  return gulp.src(./)
+  .pipe(gulp.dest('../app/public/wp-content/themes/' + base.themaFolder))
+});
 
 //Sass
 gulp.task('sass', function() {
   return gulp.src('./assets/sass/**/*.sass')
   .pipe(sass())
   .pipe(autoprefixer())
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('../app/public/wp-content/themes/newtheme'))
+});
+
+//ImageMin
+gulp.task('imageMin', function() {
+  return gulp.src('./assets/images/**/*')
+  .pipe(imageMin([
+    pngquant({
+      quality: '65-80',
+      speed: 1,
+      floyd: 0
+    }),
+    mozjpeg({
+      quality: 85,
+      progressive: true
+    }),
+    imagemin.svgo(),
+    imagemin.optipng(),
+    imagemin.gifsicle()
+  ]))
+  .pipe(gulp.dest('../app/public/wp-content/themes/newtheme')) //保存場所
 });
 
 //BrowserSync
@@ -23,4 +55,8 @@ gulp.task('watch',function() {
 });
 
 //Default
-gulp.task('default',['sass','watch']);
+gulp.task('default',[
+  'publish',
+  'sass',
+  'watch'
+]);
